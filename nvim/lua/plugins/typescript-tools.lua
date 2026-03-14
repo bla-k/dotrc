c5f33e719cee33bc481d4f7354de03d564482fe6
Davide Giuseppe Monaco <monaco.dg@gmail.com>
2026-03-14 11:57:24 +0100

    use typescript tools with (ts|js)x?

--- nvim/lua/plugins/typescript-tools.lua
+++ nvim/lua/plugins/typescript-tools.lua
@@ -1,5 +1,6 @@
 return {
     "pmizio/typescript-tools.nvim",
     dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
-    opts = {}
+    opts = {},
+    ft = { "javascript", "javascriptreact", "typescript", "typescriptreact" }
 }
