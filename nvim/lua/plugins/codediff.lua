eb36faa75513e9bfa1509008684c4a94ff97b542
bla-k <black@gouken.local>
2026-07-17 22:57:10 +0900

    plugin: codediff

---
+++ nvim/lua/plugins/codediff.lua
@@ -0,0 +1,15 @@
+return {
+    "esmuellert/codediff.nvim",
+    cmd = "CodeDiff",
+    opts = {
+        highlights = {
+            line_insert = "#2F4D42",
+            line_delete = "#4C313B",
+        },
+
+        explorer = {
+            flatten_dirs = false,
+            view_mode = "tree",
+        },
+    },
+}
